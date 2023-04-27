1e4ab5477bd5decd5f34584aa649c7cc4458d791
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-04-27 10:54:13 -0500

    Cleaned up mock provider serialize method.

--- test/Pasper.Tests/Mocking/MockProvider.cs
+++ test/Pasper.Tests/Mocking/MockProvider.cs
@@ -8,21 +8,19 @@ internal sealed class MockProvider : ISerializationProvider
     public string Serialize<T>(T input)
     {
         var properties = typeof(T).GetProperties();
-        var sb = new StringBuilder();
+        var serializedInput = new StringBuilder();
         foreach (var property in properties)
         {
             if (property.IsIgnored())
                 continue;
 
-            string key = attributes
-                .SingleOrDefault(a => typeof(ISerializedAttribute).IsAssignableFrom(a.GetType()))
-                ?.ToString()
-                ?? property.Name;
+            if (!property.IsSerialized(out string key))
+                continue;
 
-            sb.AppendFormat("{0}: {1}\n", key, property.GetValue(input));
+            serializedInput.AppendFormat("{0}: {1}\n", key, property.GetValue(input));
         }
 
-        return sb.ToString();
+        return serializedInput.ToString();
     }
 
     public T Deserialize<T>(string input) => default;
