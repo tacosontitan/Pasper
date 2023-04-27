1eb5b3e7e33b79e2d504ab87e220fa8fdc3cc128
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-04-26 21:54:02 -0500

    Created a basic implementation of SerializedAttribute.

---
+++ src/Pasper/SerializedAttribute.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Pasper;
+
+/// <summary>
+/// Defines a simplified common attribute for defining how members should be serialized.
+/// </summary>
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+public class SerializedAttribute : Attribute, ISerializedAttribute
+{
+    /// <summary>
+    /// Creates a new <see cref="SerializedAttribute"/> instance.
+    /// </summary>
+    /// <param name="name">The name of the member when serialized.</param>
+    public SerializedAttribute(string name) =>
+        Name = name;
+
+    /// <summary>
+    /// Gets or sets the name of the member when serialized.
+    /// </summary>
+    public string Name { get; set; }
+}
